4aa8771727bd816d5f0d922ba21d28b903d63042
Klaus Hebsgaard <klaus@hebsgaard.dk>
2020-03-18 21:46:26 +0100

    use gava

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ docker run -d --name gavakut \
 --rm \
 -v $PWD/helper:/app/helper \
 -v $PWD/helpee:/app/helpee \
--e "LETSENCRYPT_HOST=nextcloud.khebbie.dk" \
+-e "LETSENCRYPT_HOST=gava.khebbie.dk" \
 -e "LETSENCRYPT_EMAIL=khebbie@protonmail.com" \
--e "VIRTUAL_HOST=nextcloud.khebbie.dk" \
+-e "VIRTUAL_HOST=gava.khebbie.dk" \
 gavakut
